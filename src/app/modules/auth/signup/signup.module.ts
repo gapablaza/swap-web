6666ed764dc21ac6c3fd13aa769e789361b56005
Gerson Apablaza <gerson.eaa@gmail.com>
2023-07-24 23:55:59 -0400

    fix google button without directive

--- src/app/modules/auth/signup/signup.module.ts
+++ src/app/modules/auth/signup/signup.module.ts
@@ -4,6 +4,7 @@ import { MaterialModule } from 'src/app/shared/material.module';
 import { SignupRoutingModule } from './signup-routing.module';
 import { SharedModule, SocialModule } from 'src/app/shared';
 import { SignupComponent } from './signup.component';
+import { GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { SignupComponent } from './signup.component';
   imports: [
     SharedModule,
     SocialModule,
+    GoogleSigninButtonModule,
     MaterialModule,
     SignupRoutingModule,
   ]
