95d4939536d0875415787b9a39a61de2dec5f518
Gerson Apablaza <gerson.eaa@gmail.com>
2024-05-28 23:11:18 -0400

    2nd gen functions

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,20 @@
 'use strict';
 
-const functions = require('firebase-functions');
+const { onRequest } = require('firebase-functions/v2/https');
+const {
+  onValueDeleted,
+  onValueWritten,
+} = require('firebase-functions/v2/database');
+const { setGlobalOptions } = require('firebase-functions/v2');
+const logger = require('firebase-functions/logger');
 const admin = require('firebase-admin');
-const { DataSnapshot } = require('firebase-functions/v1/database');
-admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const { initializeApp } = require('firebase-admin/app');
+initializeApp(); // admin.initializeApp();
+
+// locate all functions closest to users
+setGlobalOptions({ region: 'us-central1', maxInstances: 10 });
+const db = admin.database();
 
 // Cut off time. Child nodes older than this will be deleted.
 const CUT_OFF_TIME = 2 * 60 * 60 * 1000; // 2 Hours in milliseconds.
@@ -16,129 +24,121 @@ const ONLINE_USERS_CUT_OFF_TIME = 2 * 24 * 60 * 60 * 1000; // 2 Days in millisec
 const MAX_OLDER_CONVERSATIONS_TIME = 365 * 24 * 60 * 60 * 1000; // 365 days in milliseconds.
 const MAX_DELETED_CONVERSATIONS = 500;
 
+
+// 2nd Gen
 // Función para notificar nuevos mensajes al destinatario
-exports.notifyNewMessage = functions.database
-  .ref('/unreadUserMessages/{userId}/{otherUserId}')
-  .onWrite(async (change, context) => {
-    const userId = context.params.userId.substring(7);
-    const otherUserId = context.params.otherUserId.substring(7);
-    
-    if (!change.after.val()) {
-      return functions.logger.log(
-        'notifyNewMessage: Mensaje de ',
-        otherUserId,
-        ' ya leído por ',
-        userId
-      );
+exports.notifyNewMessage2ndGen = onValueWritten(
+  '/unreadUserMessages/{userId}/{otherUserId}',
+  async (event) => {
+    const userId = event.params.userId.substring(7);
+    const otherUserId = event.params.otherUserId.substring(7);
+
+    // Exit when the data is deleted.
+    if (!event.data.after.exists()) {
+      return null;
     }
 
     // Se verifica que el "user" no tenga en su blacklist al "otherUser"
-    const blacklistSnapshot = await admin
-      .database()
+    const blacklistSnapshot = await db
       .ref(`/userBlacklist/userId_${userId}/userId_${otherUserId}`)
       .once('value');
 
-    if (blacklistSnapshot.val() && blacklistSnapshot.val() == true) {
-      return functions.logger.log(`userId_${userId} tiene en su blacklist al userId_${otherUserId}`);
-    } 
+    if (blacklistSnapshot.exists() && blacklistSnapshot.val() == true) {
+      return logger.info(
+        `[2G] userId_${userId} tiene en su blacklist al userId_${otherUserId}`
+      );
+    }
 
-    functions.logger.log(
-      'El usuario ',
+    logger.info(
+      '[2G] El usuario ID',
       userId,
-      ' tiene un nuevo mensaje de ',
+      ' tiene un nuevo mensaje del usuario ID ',
       otherUserId
     );
 
     // Get the list of device notification tokens.
-    const getDeviceTokensPromise = admin
-      .database()
+    const getDeviceTokensPromise = db
       .ref(`/users/userId_${userId}/notificationTokens`)
       .once('value');
 
-    // Get the other user profile.
-    // const getFollowerProfilePromise = admin.auth().getUser(followerUid);
-
-    // The snapshot to the user's tokens.
-    let tokensSnapshot;
-
-    // The array containing all the user's tokens.
-    let tokens;
-
     const results = await Promise.all([getDeviceTokensPromise]);
-    tokensSnapshot = results[0];
-    // const follower = results[1];
+    const tokensSnapshot = results[0];
 
     // Check if there are any device tokens.
     if (!tokensSnapshot.hasChildren()) {
-      return functions.logger.log(
-        'There are no notification tokens to send to.'
+      return logger.log(
+        '[2G] There are no notification tokens to send to.'
       );
     }
-    functions.logger.log(
-      'There are',
+
+    logger.log(
+      '[2G] There are',
       tokensSnapshot.numChildren(),
       'tokens to send notifications to.'
     );
-    // functions.logger.log('Fetched follower profile', follower);
 
-    // Notification details.
-    const payload = {
-      notification: {
-        title: `${change.after.val().fromUserName} dice:`,
-        body: `${change.after.val().body}`,
-        icon: `${change.after.val().fromUserImage}`,
-        color: '#46be9c',
-        click_action: `https://intercambialaminas.com/message/${otherUserId}`,
-      },
+    const notificationData = {
+      title: `${event.data.after.val().fromUserName} dice:`,
+      body: `${event.data.after.val().body}`,
+      // icon: `${event.data.after.val().fromUserImage}`,
+      // color: '#46be9c',
+      // click_action: `https://intercambialaminas.com/message/${otherUserId}`,
     };
 
     // Listing all tokens as an array.
-    tokens = Object.keys(tokensSnapshot.val());
-    // Send notifications to all tokens.
-    const response = await admin.messaging().sendToDevice(tokens, payload);
+    const tokens = Object.keys(tokensSnapshot.val());
+
+    // Send message to all tokens
+    const response = await admin.messaging().sendEachForMulticast({
+      tokens: tokens,
+      notification: notificationData,
+      // data: {
+      //     ownerId: ownerId,
+      //     userId: userId,
+      //     notificationOption: "receivedFriendRequest",
+      // },
+    });
+
     // For each message check if there was an error.
     const tokensToRemove = [];
-    response.results.forEach((result, index) => {
-      const error = result.error;
-      if (error) {
-        functions.logger.error(
-          'Failure sending notification to',
-          tokens[index],
-          error
-        );
-        // Cleanup the tokens who are not registered anymore.
-        if (
-          error.code === 'messaging/invalid-registration-token' ||
-          error.code === 'messaging/registration-token-not-registered'
-        ) {
-          tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
+    response.responses.forEach((result, index) => {
+        const error = result.error;
+        if (error) {
+            logger.error(
+                'Failure sending notification to',
+                tokens[index],
+                error.code,
+                // error.message
+            );
+            // Cleanup the tokens who are not registered anymore.
+            if (error.code === 'messaging/unregistered' || 
+                error.code === 'messaging/invalid-argument') {               
+                tokensToRemove.push(
+                  tokensSnapshot.ref.child(tokens[index]).remove()
+                );
+            }
         }
-      }
     });
     return Promise.all(tokensToRemove);
-  });
+  }
+);
 
+
+// 2nd Gen
 // Función para actualizar los userResume cuando se elimina un mensaje sin leer
-exports.updateUnreadStatus = functions.database
-  .ref('/unreadUserMessages/{userId}/{otherUserId}')
-  .onDelete(async (change, context) => {
-    const userId = context.params.userId;
-    const otherUserId = context.params.otherUserId;
+exports.updateUnreadStatus2ndGen = onValueDeleted(
+  '/unreadUserMessages/{userId}/{otherUserId}',
+  async (event) => {
+    const userId = event.params.userId;
+    const otherUserId = event.params.otherUserId;
 
-    functions.logger.log(
-      'updateUnreadStatus: De ',
-      otherUserId,
-      ' para ',
-      userId
-    );
+    logger.info('[2G] UpdateUnreadStatus: De ', otherUserId, ' para ', userId);
 
     let updates = {};
-    const userResumeRef = admin.database().ref('/userResume');
-
+    const userResumeRef = db.ref('/userResume');
     const userRes = await userResumeRef
       .child(userId + '/' + otherUserId)
       .once('value');
-
     const otherRes = await userResumeRef
       .child(otherUserId + '/' + userId)
       .once('value');
@@ -149,13 +149,17 @@ exports.updateUnreadStatus = functions.database
 
       return userResumeRef.update(updates);
     } else {
-      return functions.logger.log('No existen los nodos de los usuarios');
+      return logger.info('No existen los nodos de los usuarios');
     }
-  });
+  }
+);
 
+
+// 2nd Gen
 // Función que elimina conversaciones con una antigüedad mayor a la configurada (1 año)
-exports.deleteOldConversations = functions.https.onRequest(
-  async (request, response) => {
+exports.deleteOldConversations2ndGen = onRequest(
+  { cors: true, timeoutSeconds: 300 },
+  async (req, res) => {
     const now = Date.now();
     const cutoff = now - MAX_OLDER_CONVERSATIONS_TIME;
 
@@ -163,12 +167,11 @@ exports.deleteOldConversations = functions.https.onRequest(
     let deletes = {};
     let promises = [];
 
-    const mainRef = admin.database().ref();
-    const userResumeRef = admin.database().ref('/userResume');
+    const mainRef = db.ref();
+    const userResumeRef = db.ref('/userResume');
 
-    // const queryUserResume = userResumeRef.limitToFirst(1000);
-    // const snapshotUserResumen = await queryUserResume.once('value');
     const snapshotUserResumen = await userResumeRef.once('value');
+    logger.info('number of nodes: ', snapshotUserResumen.numChildren());
 
     snapshotUserResumen.forEach((mainNode) => {
       if (counter == MAX_DELETED_CONVERSATIONS) {
@@ -177,7 +180,6 @@ exports.deleteOldConversations = functions.https.onRequest(
 
       // 1.- Obtener el nodo de resumen por usuario /userResume
       let mainUserKey = mainNode.key;
-      // functions.logger.info('mainUserKey:', mainUserKey);
 
       // 2.- Recorrerlo y por cada usuario guardar su ID (mainUserId) para iterar sus conversaciones
       mainNode.forEach((childNode) => {
@@ -186,8 +188,8 @@ exports.deleteOldConversations = functions.https.onRequest(
         }
 
         let childUserKey = childNode.key;
-        // functions.logger.info('childUserKey:', childUserKey);
         let tempMessage = childNode.val();
+
         // 3.- Si la conversación tiene mas de 1 año de antigüedad se guarda el ID del childUserId
         if (tempMessage.timestamp < cutoff) {
           if (!mainUserKey || !childUserKey) {
@@ -214,8 +216,9 @@ exports.deleteOldConversations = functions.https.onRequest(
 
           promises.push(mainRef.update(deletes));
           counter++;
-          functions.logger.info(
-            'counter:',
+
+          logger.info(
+            'counter: ',
             counter,
             'node: ' + mainUserKey + '/' + childUserKey
           );
@@ -224,53 +227,32 @@ exports.deleteOldConversations = functions.https.onRequest(
       });
     });
 
-    Promise.all(promises).then(() => {
-      functions.logger.info('finish ', promises.length, ' nodes');
-    });
+    await Promise.all(promises)
+      .then(() => {
+        logger.info('finish ', promises.length, ' nodes');
+      })
+      .catch((error) => {
+        return res.status(400).json({
+          status: 'error',
+          message: error.message,
+        });
+      });
 
-    response.sendStatus(200);
+    return res.status(200).json({
+      status: 'success',
+      message: 'Delete Old Conversations Successful',
+      data: promises.length,
+    });
   }
 );
 
-// Función para eliminar nodes que no tengan la estructura correcta
-exports.deleteOldNodes = functions.https.onRequest(
-  async (request, response) => {
-    const feedRef = admin.database().ref('/userResume');
 
-    const now = Date.now();
-    const cutoff = now - CUT_OFF_TIME;
-    const query = feedRef
-      // .orderByChild('timestamp')
-      // .endAt(cutoff)
-      .limitToFirst(200);
-
-    const snapshot = await query.once('value');
-    // create a map with all children that need to be removed
-    const updates = {};
-    let nodes = [];
-    snapshot.forEach((child) => {
-      if (!child.key.startsWith('userId_')) {
-        updates[child.key] = null;
-        nodes.push(child.key);
-      }
-    });
-
-    // execute all updates in one go and return the result to end the function
-    // return feedRef.update(updates);
-    functions.logger.info('nodes:', nodes);
-    response.send(feedRef.update(updates));
-    // response.send(nodes);
-  }
-);
-
+// 2nd Gen
 // Función para eliminar registros antiguos del /feedsHome
-exports.cleanFeed = functions
-  .runWith({
-    timeoutSeconds: 300,
-  })
-  .https.onRequest(async (request, response) => {
-    const feedRef = admin.database().ref('/feedsHome');
-
+exports.cleanFeed2ndGen = onRequest(
+  { cors: true, timeoutSeconds: 300 },
+  async (req, res) => {
+    const feedRef = db.ref('/feedsHome');
     const now = Date.now();
     const cutoff = now - CUT_OFF_TIME;
     const oldItemsQuery = feedRef
@@ -279,26 +261,39 @@ exports.cleanFeed = functions
       .limitToFirst(500);
 
     const snapshot = await oldItemsQuery.once('value');
+    logger.info('number of nodes: ', snapshot.numChildren());
+
     // create a map with all children that need to be removed
     const updates = {};
     snapshot.forEach((child) => {
       updates[child.key] = null;
     });
+    logger.info('Updates count: ', Object.keys(updates).length);
 
     // execute all updates in one go and return the result to end the function
-    // return feedRef.update(updates);
-    functions.logger.info('updates count:', snapshot.numChildren());
-    response.send(feedRef.update(updates));
-  });
+    // response.send(feedRef.update(updates));
+    await feedRef.update(updates).catch((error) => {
+      return res.status(400).json({
+        status: 'error',
+        message: error.message,
+      });
+    });
 
+    return res.status(200).json({
+      status: 'success',
+      message: 'Clean Feed Successful',
+      data: Object.keys(updates).length,
+    });
+  }
+);
+
+
+// 2nd Gen
 // Función para eliminar registros antiguos del /onlineUsers
-exports.cleanOnlineUsers = functions
-  .runWith({
-    timeoutSeconds: 300,
-  })
-  .https.onRequest(async (request, response) => {
-    const onlineUsersRef = admin.database().ref('/onlineUsers');
-
+exports.cleanOnlineUsers2ndGen = onRequest(
+  { cors: true, timeoutSeconds: 300 },
+  async (req, res) => {
+    const onlineUsersRef = db.ref('/onlineUsers');
     const now = Date.now();
     const cutoff = now - ONLINE_USERS_CUT_OFF_TIME;
     const onlineUsersQuery = onlineUsersRef
@@ -307,6 +302,8 @@ exports.cleanOnlineUsers = functions
       .limitToFirst(500);
 
     const snapshot = await onlineUsersQuery.once('value');
+    logger.info('number of nodes: ', snapshot.numChildren());
+
     // create a map with all children that need to be removed
     const updates = {};
     snapshot.forEach((child) => {
@@ -315,9 +312,48 @@ exports.cleanOnlineUsers = functions
         updates[child.key] = null;
       }
     });
+    logger.info('Updates count: ', Object.keys(updates).length);
 
     // execute all updates in one go and return the result to end the function
-    // return onlineUsersRef.update(updates);
-    functions.logger.info('updates count:', snapshot.numChildren());
-    response.send(onlineUsersRef.update(updates));
-  });
+    await onlineUsersRef.update(updates).catch((error) => {
+      return res.status(400).json({
+        status: 'error',
+        message: error.message,
+      });
+    });
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Clean Online Users Successful',
+      data: Object.keys(updates).length,
+    });
+  }
+);
+
+
+// 2nd Gen
+// Función para insertar un nuevo registro en el Home's Feed /feedsHome
+exports.addFeed = onRequest(
+  { cors: true, timeoutSeconds: 300 },
+  async (req, res) => {
+    const feedsHomeRef = db.ref('/feedsHome');
+    const feedData = req.body;
+    logger.info('data: ', JSON.stringify(feedData));
+
+    if (req.method !== 'POST') {
+      res.status(400).json('Bad request!');
+      return;
+    }
+
+    try {
+      await feedsHomeRef.push(feedData);
+
+      res.status(200).json({
+        status: 'success',
+        message: 'Home Feed Added Successful',
+      });
+    } catch (error) {
+      res.status(500).json('We found an error posting your request!');
+    }
+  }
+);
